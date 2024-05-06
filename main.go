8faaec9db01960347785c8f08108e29ee7a2bb2b
SlaviXG <slavix.com@gmail.com>
2024-05-06 15:51:25 +0300

    feat: print request path to console

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ import (
 )
 
 func main() {
-	/
+	// Running this example will spin up a server on port 8080,
+	// and can be accessed at http://localhost:8080
+
 	fmt.Println("Handling started...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		path := html.EscapeString(r.URL.Path)
+		fmt.Fprintf(w, "Received request: %q", path)
+		//For more information: fmt.Println(r.Header)
+		fmt.Printf("Received request: %q\n", path)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
